df4d8b5fdba8be6bebd77367305e859484d5af99
Samol <amsamoll@gmail.com>
2024-09-12 10:48:27 +0700

    sub category

---
+++ client/src/router/index.ts
@@ -0,0 +1,142 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import FrontLayout from '../layouts/frontLayout.vue'
+import AdminLayout from '../layouts/adminLayout.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '',
+      name: 'front.layout',
+      component: FrontLayout,
+      meta: {
+        requiresAuth: false
+      },
+      children: [
+        {
+          path: '',
+          name: 'home',
+          component: () => import('@/views/modules/frontend/HomeView.vue')
+        }
+      ]
+    },
+    {
+      path: '/admin/login',
+      name: 'admin.users.login',
+      meta: {
+        requiresAuth: false
+      },
+      component: () => import('@/views/modules/login/login.vue')
+    },
+    {
+      path: '/admin',
+      name: 'admin.layout',
+      component: AdminLayout,
+      meta: {
+        requiresAuth: true
+      },
+      children: [
+        {
+          path: 'dashboard',
+          name: 'admin.dashboard.index',
+          component: () => import('@/views/modules/backend/dashboard/dashboard.vue')
+        },
+        {
+          path: "users",
+         children: [
+          {
+            path: '',
+            name: 'admin.users.index',
+            component: () => import('@/views/modules/backend/user/index.vue')
+          },
+          {
+            path: 'create',
+            name: 'admin.users.create',
+            component: () => import("@/views/modules/backend/user/create.vue")
+          },
+          {
+            path: ':id/edit',
+            name: 'admin.users.edit',
+            component: () => import("@/views/modules/backend/user/update.vue")
+          }
+         ]
+        },
+        {
+          path: 'roles',
+          children: [
+            {
+              path: '',
+              name: 'admin.roles.index',
+              component: () => import("@/views/modules/backend/role/index.vue")
+            },
+            {
+              path: 'create',
+              name: 'admin.roles.create',
+              component: () => import("@/views/modules/backend/role/create.vue")
+            },
+            {
+              path: ':id/edit',
+              name: 'admin.roles.edit',
+              component: () => import("@/views/modules/backend/role/edit.vue")
+            }
+          ]
+        },
+        {
+          path: 'categories',
+          children: [
+            {
+              path: '',
+              name: 'admin.categories.index',
+              component: () => import("@/views/modules/backend/category/index.vue")
+            },
+            {
+              path: 'create',
+              name: 'admin.categories.create',
+              component: () => import("@/views/modules/backend/category/create.vue")
+            },
+            {
+              path: ':id/edit',
+              name: 'admin.categories.edit',
+              component: () => import("@/views/modules/backend/category/edit.vue")
+            }
+          ]
+        },
+        {
+          path: 'sub-categories',
+          children: [
+            {
+              path: '',
+              name: 'admin.subcategories.index',
+              component: () => import("@/views/modules/backend/sub_category/index.vue")
+            },
+            {
+              path: 'create',
+              name: 'admin.subcategories.create',
+              component: () => import("@/views/modules/backend/sub_category/create.vue")
+            },
+            {
+              path: ':id/edit',
+              name: 'admin.subcategories.edit',
+              component: () => import("@/views/modules/backend/sub_category/edit.vue")
+            }
+          ]
+        }
+      ]
+    }
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = localStorage.getItem('isAuth');
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  
+  if (requiresAuth && !isAuthenticated) {
+    next('/admin/login');
+  } else if (isAuthenticated && to.path === '/admin/login') {
+    next('/admin/dashboard');
+  } else {
+    next();
+  }
+});
+
+export default router
